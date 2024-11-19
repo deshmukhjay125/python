3ae835ba8d21baf3289b7b08011e8d969573e9e5
deshmukhjay125 <152011312+deshmukhjay125@users.noreply.github.com>
2024-11-19 17:35:38 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,41 @@
+# a = 1234,4545
+# b = 9745,56565
+
+# # print(f"addition of complex {complex(a)+complex(b)}")
+
+# # print(f"addition of float {float(a)+float(b)}")
+# # print(f"sub of float {float(a)-float(b)}")
+
+# # print(f"addition of int {int(a)+int(b)}")
+
+# # print(str(a))
+# # print(str(b))
+# # print(f"addition of str {str(a)+str(b)}")
+
+# print(list(a))
+# print(list(b))
+
+# print(tuple(a))
+# print(tuple(b))
+
+# # print(range(a))
+# # print(range(b))
+
+# # print(range(a)+range(b))
+
+# print(dict(a,b))
+# print(dict(b))
+# print(dict(a)+ dict(b))
+
+# print(set(a))
+# print(set(b))
+# print(set(a)+ set(b))
+
+# # DA -> powerBI, SQL, Adv Excel
+# # DS -> Python + Adv_Python, ML, World CloudAI
+
+
+# # print(bool(a))
+# # print(bool(b))
+# # print(bool(a)+bool(b))
+
